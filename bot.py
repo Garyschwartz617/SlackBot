bc8090a43e1c41a117ff163af7a97eaba0f1716a
Garyschwartz617 <gary.s.schwartz617@gmail.com>
2021-11-12 01:43:48 +0200

    all done

--- bot.py
+++ bot.py
@@ -5,88 +5,104 @@ from dotenv import load_dotenv
 from flask import Flask, Response, request
 from slackeventsapi import SlackEventAdapter
 from twitter import get_my_tweets,get_python_tweets
-import datetime
+import datetime, time
 
 
+# sets up path
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-
+# set up Flask
 app = Flask(__name__)
-slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
 
+# sets up slack so we can use the code
+slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/events',app)
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
+a = True
+# need this loop bc it constantly allows us to check our if statement and if it matches the time
+while a:
+
+    #  constantly checking time and if it matches then it sends out the hourly reminder
+    now = datetime.datetime.now()
+    if datetime.datetime.now().minute == 7 and  0 <= datetime.datetime.now().second <= 5  :
+        client.chat_postMessage(channel='#api', text = f'your Hourly reminder at {now}') 
+        time.sleep(5)   
 
 
+    # allows us to count messages per user
+    message_counts = {}
 
-message_counts = {}
+    @slack_event_adapter.on('message')
+    def message(payload):
+        event = payload.get('event', {})
+        channel_id = event.get('channel')
+        user_id = event.get('user')
+        text = event.get('text')
 
-@slack_event_adapter.on('message')
-def message(payload):
-    event = payload.get('event', {})
-    channel_id = event.get('channel')
-    user_id = event.get('user')
-    text = event.get('text')
+        if user_id != BOT_ID:
+            if user_id in message_counts:
+                message_counts[user_id] += 1
+            else:
+                message_counts[user_id] = 1
 
-    if user_id != BOT_ID:
-        if user_id in message_counts:
-            message_counts[user_id] += 1
-        else:
-            message_counts[user_id] = 1
-   
-        client.chat_postMessage(channel=channel_id, text= text)
+            client.chat_postMessage(channel=channel_id, text= text)
 
-@app.route('/message-count', methods =['POST'])
-def message_count():
-    data = request.form
-    user_id = data.get('user_id')
-    channel_id = data.get('channel_id')
-    message_count = message_counts.get(user_id,0)
-    client.chat_postMessage(channel=channel_id, text = f'message: {message_count}')
-    return Response(), 200
+    # lets us print out the hour message whenever we would like
+    @app.route('/hour-message', methods =['POST'])
+    def hour_message(): 
+        print("Doing stuff...")
+        date = datetime.datetime.now()
+        client.chat_postMessage(channel='#api', text = f'your Hourly reminder at {date}')    
+        return Response(), 200
+        
+    # tells us how many messages we have sent out
+    @app.route('/message-count', methods =['POST'])
+    def message_count():
+        data = request.form
+        user_id = data.get('user_id')
+        channel_id = data.get('channel_id')
+        message_count = message_counts.get(user_id,0)
+        client.chat_postMessage(channel=channel_id, text = f'message: {message_count}')
+        return Response(), 200
 
-@app.route('/python-tweets', methods =['POST'])
-def python_tweets():
+    # Sends us all tweets from these endpoints if we request them
+    @app.route('/python-tweets', methods =['POST'])
+    def python_tweets():
 
-    data = request.form
-    channel_id = data.get('channel_id')
-    tweets = get_python_tweets()
+        data = request.form
+        channel_id = data.get('channel_id')
+        tweets = get_python_tweets()
 
-    for key , values in tweets.items():
-        if values == []:
-            1
-        else:
-            for value in values:
-                for k, v in value.items():
-                    print(f'keys : {key} , value {k}')
-                    client.chat_postMessage(channel=channel_id, text = f'{key}s tweets   {k}, time {v}')
-    return Response(), 200
-
-
-time_since  = {'time' :datetime.datetime(2000,1,1,0,0,0)}
-
-@app.route('/my-tweets', methods =['POST'])
-def my_tweets():
-    
-
-    data = request.form
-    channel_id = data.get('channel_id')
-    tweets = get_my_tweets()
-    tweets.reverse()
-    for tweet in tweets:
-        print(f' tweets {tweet[1]}')
-        if time_since['time'] < tweet[1]:
-            print(f' time since {time_since}')
-
-            print(f' tweets {tweet[0]}')
-            client.chat_postMessage(channel=channel_id, text = f'my new tweet - {tweet[0]}')
-            time_since['time'] = tweet[1]
-        else:
-            1    
-    return Response(), 200
+        for key , values in tweets.items():
+            if values == []:
+                1
+            else:
+                for value in values:
+                    for k, v in value.items():
+                        client.chat_postMessage(channel=channel_id, text = f'{key}s tweets   {k}, time {v}')
+        return Response(), 200
 
 
+    # Sends all my new tweets out, and keeps track of my oldest tweet
+    time_since  = {'time' :datetime.datetime(2000,1,1,0,0,0)}
 
-if __name__ == "__main__":
-    app.run(debug=True)
+    @app.route('/my-tweets', methods =['POST'])
+    def my_tweets():
+        data = request.form
+        channel_id = data.get('channel_id')
+        tweets = get_my_tweets()
+        tweets.reverse()
+        for tweet in tweets:
+            print(f' tweets {tweet[1]}')
+            if time_since['time'] < tweet[1]:
+                client.chat_postMessage(channel=channel_id, text = f'my new tweet - {tweet[0]}')
+                time_since['time'] = tweet[1]
+            else:
+                1    
+        return Response(), 200
+
+
+    time.sleep(1)
+    if __name__ == "__main__":
+        app.run(debug=True)
--- twitter.py
+++ twitter.py
@@ -5,90 +5,18 @@ from pathlib import Path
 from dotenv import load_dotenv
 import datetime
 
-# To set your enviornment variables in your terminal run the following line:
-# export 'CONSUMER_KEY'='<your_consumer_key>'
-# export 'CONSUMER_SECRET'='<your_consumer_secret>'
-
+# loading us the path so we can get to the token down below
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
 
-
+# all the keys and tokens we need to sign in
 consumer_key = os.environ.get("CONSUMER_KEY")
 consumer_secret = os.environ.get("CONSUMER_SECRET")
 access_token = os.environ.get("ACCESS_TOKEN")
 access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
 
-
-# You can adjust ids to include a single Tweets
-# Or you can add to up to 100 comma-separated IDs
-# id = "745911914"
-# params = {"ids": id, "tweet.fields": "created_at"}
-
-# fields = "created_at,description"
-# params = {"usernames": "PythonWeekly,fullstackpython,realpython", "user.fields": fields}
-
-# Tweet fields are adjustable.
-# Options include:
-# attachments, author_id, context_annotations,
-# conversation_id, created_at, entities, geo, id,
-# in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
-# possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
-# source, text, and withheld
-
-# request_token_url = "https://api.twitter.com/oauth/request_token"
-# oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
-
-# try:
-#     fetch_response = oauth.fetch_request_token(request_token_url)
-# except ValueError:
-#     print(
-#         "There may have been an issue with the consumer_key or consumer_secret you entered."
-#     )
-
-# resource_owner_key = fetch_response.get("oauth_token")
-# resource_owner_secret = fetch_response.get("oauth_token_secret")
-# print("Got OAuth token: %s" % resource_owner_key)
-
-# # Get authorization
-# base_authorization_url = "https://api.twitter.com/oauth/authorize"
-# authorization_url = oauth.authorization_url(base_authorization_url)
-# print("Please go here and authorize: %s" % authorization_url)
-# verifier = input("Paste the PIN here: ")
-
-# # Get the access token
-# access_token_url = "https://api.twitter.com/oauth/access_token"
-# oauth = OAuth1Session(
-#     consumer_key,
-#     client_secret=consumer_secret,
-#     resource_owner_key=resource_owner_key,
-#     resource_owner_secret=resource_owner_secret,
-#     verifier=verifier,
-# )
-# oauth_tokens = oauth.fetch_access_token(access_token_url)
-
-
-# access_token = oauth_tokens["oauth_token"]
-# access_token_secret = oauth_tokens["oauth_token_secret"]
-
-# consumer_key = os.environ.get("CONSUMER_KEY")
-# consumer_secret = os.environ.get("CONSUMER_SECRET")
-# access_token = os.environ.get("ACCESS_TOKEN")
-# access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
-
-# def create_url():
-#     # Replace with user ID below
-#     user_id = 2244994945
-#     return "https://api.twitter.com/2/users/{}/tweets".format(user_id)
-
-
+# the paramters of each tweet we would like
 def get_params():
-    # Tweet fields are adjustable.
-    # Options include:
-    # attachments, author_id, context_annotations,
-    # conversation_id, created_at, entities, geo, id,
-    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
-    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
-    # source, text, and withheld
     return {"tweet.fields": "created_at"}
 
 
@@ -101,46 +29,27 @@ oauth = OAuth1Session(
     resource_owner_secret=access_token_secret,
 )
 
-# response = oauth.get(
-#     "https://api.twitter.com/2/users/by", params=params
-# )
+# gets us the url with the value inputed to correct destination
+def get_url(value):
+    url = "https://api.twitter.com/2/users/{}/tweets".format(value)
+    response = oauth.get(
+    url, params=get_params()
+    )
+    if response.status_code != 200:
+        raise Exception(
+            "Request returned an error: {} {}".format(response.status_code, response.text)
+        )
+    json_response = response.json()
+    return json_response
 
-# response = oauth.get(
-#     create_url(), params=get_params()
-# )
 
 
-# if response.status_code != 200:
-#     raise Exception(
-#         "Request returned an error: {} {}".format(response.status_code, response.text)
-#     )
-
-# print("Response code: {}".format(response.status_code))
-# json_response = response.json()
-# # print(json.dumps(json_response, indent=4, sort_keys=True))
-# a = json_response['data'][0]['text']
-# b = json_response['data'][0]['created_at'].split('T')
-# c = b[1].split('.')
-# c = c[0].split(':')
-# b = b[0].split('-')
-# b = datetime.datetime(int(b[0]),int(b[1]),int(b[2]),int(c[0]),int(c[1]),int(c[2]))
-# e = [a,b]
-# print(e)
-
+# gets all the python tweets from the past 5 hours, parses the data so we can keep track of date efficently in datetime, and returns the text and date
 def get_python_tweets():
     user_ids ={'Python Weekly ':373620985, 'Real Python' : 745911914, 'Full Stack Python' :  2996502625}
     fun = {}
     for key , value in user_ids.items():
-        url = "https://api.twitter.com/2/users/{}/tweets".format(value)
-        response = oauth.get(
-        url, params=get_params()
-        )
-        if response.status_code != 200:
-            raise Exception(
-                "Request returned an error: {} {}".format(response.status_code, response.text)
-            )
-        # print("Response code: {}".format(response.status_code))
-        json_response = response.json()
+        json_response = get_url(value)
         fun[key] = []
         for x in range(len(json_response['data'])):
             a = json_response['data'][x]['text']
@@ -156,26 +65,13 @@ def get_python_tweets():
             if b > future_date_and_time:
                 e = {a:b}
                 # print(e)
-                fun[key].append(e)
-    print(fun)    
- 
-    return fun            
-print('HIiIIIIIII')
-# get_python_tweets()
-
-
+                fun[key].append(e) 
+    return fun   
+             
+# gets all of my tweets with their dates
 def get_my_tweets():
     value = 1458580045702873089
-    url = "https://api.twitter.com/2/users/{}/tweets".format(value)
-    response = oauth.get(
-    url, params=get_params()
-    )
-    if response.status_code != 200:
-        raise Exception(
-            "Request returned an error: {} {}".format(response.status_code, response.text)
-        )
-    # print("Response code: {}".format(response.status_code))
-    json_response = response.json()
+    json_response = get_url(value)
     lst = []
     for x in range(len(json_response['data'])):
         a = json_response['data'][x]['text']
@@ -186,6 +82,4 @@ def get_my_tweets():
         b = datetime.datetime(int(b[0]),int(b[1]),int(b[2]),int(c[0]),int(c[1]),int(c[2]))
         e = [a,b]
         lst.append(e)
-            # print(e)
     return lst        
-# get_my_tweets()
