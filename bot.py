55450b619f1b9bb68be085a04fb295e69fccf9a4
Garyschwartz617 <gary.s.schwartz617@gmail.com>
2021-11-11 11:35:30 +0200

    twitter added

--- bot.py
+++ bot.py
@@ -2,7 +2,7 @@ import slack
 import os
 from pathlib import Path
 from dotenv import load_dotenv
-from flask import Flask
+from flask import Flask, Response, request
 from slackeventsapi import SlackEventAdapter
 
 env_path = Path('.') / '.env'
@@ -14,18 +14,61 @@ slack_event_adapter = SlackEventAdapter(os.environ['SIGNING_SECRET'],'/slack/eve
 client = slack.WebClient(token=os.environ['SLACK_TOKEN'])
 BOT_ID = client.api_call("auth.test")['user_id']
 
+import asyncio,datetime
 
-import sched, time, datetime
-s = sched.scheduler(time.time, time.sleep)
-def do_something(sc): 
-    print("Doing stuff...")
+
+
+async def some_function():
     date = datetime.datetime.now()
+
     client.chat_postMessage(channel='#api', text = f'your Hourly reminder at {date}')
-    s.enter(60, 1, do_something, (sc,))
 
-s.enter(60, 1, do_something, (s,))
-s.run()
+    # asynchronous sleep of 1 second
 
+async def forever():
+    while True:
+        await asyncio.sleep(10)
+
+        await some_function()
+
+loop = asyncio.get_event_loop()
+loop.run_until_complete(forever())
+
+
+
+
+
+
+# async def hour_reminder():
+#     print('Hello')
+#     date = datetime.datetime.now()
+
+#     client.chat_postMessage(channel='#api', text = f'your Hourly reminder at {date}')
+
+#     # asynchronous sleep of 1 second
+#     await asyncio.sleep(10)
+#     print('World')
+
+# loop = asyncio.get_event_loop()
+# # we run our coroutine in the event loop until it is completed
+# loop.run_forever(hour_reminder())
+# # close the event loop
+# # loop.close()
+
+
+
+# import sched, time, datetime
+# s = sched.scheduler(time.time, time.sleep)
+# def do_something(sc): 
+#     print("Doing stuff...")
+#     date = datetime.datetime.now()
+#     client.chat_postMessage(channel='#api', text = f'your Hourly reminder at {date}')
+#     s.enter(60, 1, do_something, (sc,))
+
+# s.enter(60, 1, do_something, (s,))
+# s.run()
+
+message_counts = {}
 
 @slack_event_adapter.on('message')
 def message(payload):
@@ -35,7 +78,26 @@ def message(payload):
     text = event.get('text')
 
     if user_id != BOT_ID:
+        if user_id in message_counts:
+            message_counts[user_id] += 1
+        else:
+            message_counts[user_id] = 1
+   
         client.chat_postMessage(channel=channel_id, text= text)
+
+@app.route('/message-count', methods =['POST'])
+def message_count():
+    data = request.form
+    user_id = data.get('user_id')
+    channel_id = data.get('channel_id')
+    message_count = message_counts.get(user_id,0)
+    client.chat_postMessage(channel=channel_id, text = f'message: {message_count}')
+    return Response(), 200
+
+
+
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ twitter.py
@@ -0,0 +1,117 @@
+from requests_oauthlib import OAuth1Session
+import os
+import json
+from pathlib import Path
+from dotenv import load_dotenv
+
+# To set your enviornment variables in your terminal run the following line:
+# export 'CONSUMER_KEY'='<your_consumer_key>'
+# export 'CONSUMER_SECRET'='<your_consumer_secret>'
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+consumer_key = os.environ.get("CONSUMER_KEY")
+consumer_secret = os.environ.get("CONSUMER_SECRET")
+access_token = os.environ.get("ACCESS_TOKEN")
+access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+
+
+# You can adjust ids to include a single Tweets
+# Or you can add to up to 100 comma-separated IDs
+# id = "745911914"
+# params = {"ids": id, "tweet.fields": "created_at"}
+fields = "created_at,description"
+params = {"usernames": "PythonWeekly,fullstackpython,realpython", "user.fields": fields}
+# Tweet fields are adjustable.
+# Options include:
+# attachments, author_id, context_annotations,
+# conversation_id, created_at, entities, geo, id,
+# in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
+# possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
+# source, text, and withheld
+
+# request_token_url = "https://api.twitter.com/oauth/request_token"
+# oauth = OAuth1Session(consumer_key, client_secret=consumer_secret)
+
+# try:
+#     fetch_response = oauth.fetch_request_token(request_token_url)
+# except ValueError:
+#     print(
+#         "There may have been an issue with the consumer_key or consumer_secret you entered."
+#     )
+
+# resource_owner_key = fetch_response.get("oauth_token")
+# resource_owner_secret = fetch_response.get("oauth_token_secret")
+# print("Got OAuth token: %s" % resource_owner_key)
+
+# # Get authorization
+# base_authorization_url = "https://api.twitter.com/oauth/authorize"
+# authorization_url = oauth.authorization_url(base_authorization_url)
+# print("Please go here and authorize: %s" % authorization_url)
+# verifier = input("Paste the PIN here: ")
+
+# # Get the access token
+# access_token_url = "https://api.twitter.com/oauth/access_token"
+# oauth = OAuth1Session(
+#     consumer_key,
+#     client_secret=consumer_secret,
+#     resource_owner_key=resource_owner_key,
+#     resource_owner_secret=resource_owner_secret,
+#     verifier=verifier,
+# )
+# oauth_tokens = oauth.fetch_access_token(access_token_url)
+
+
+# access_token = oauth_tokens["oauth_token"]
+# access_token_secret = oauth_tokens["oauth_token_secret"]
+
+# consumer_key = os.environ.get("CONSUMER_KEY")
+# consumer_secret = os.environ.get("CONSUMER_SECRET")
+# access_token = os.environ.get("ACCESS_TOKEN")
+# access_token_secret = os.environ.get("ACCESS_TOKEN_SECRET")
+
+def create_url():
+    # Replace with user ID below
+    user_id = 2244994945
+    return "https://api.twitter.com/2/users/{}/tweets".format(user_id)
+
+
+def get_params():
+    # Tweet fields are adjustable.
+    # Options include:
+    # attachments, author_id, context_annotations,
+    # conversation_id, created_at, entities, geo, id,
+    # in_reply_to_user_id, lang, non_public_metrics, organic_metrics,
+    # possibly_sensitive, promoted_metrics, public_metrics, referenced_tweets,
+    # source, text, and withheld
+    return {"tweet.fields": "created_at"}
+
+
+
+# Make the request
+oauth = OAuth1Session(
+    consumer_key,
+    client_secret=consumer_secret,
+    resource_owner_key=access_token,
+    resource_owner_secret=access_token_secret,
+)
+
+# response = oauth.get(
+#     "https://api.twitter.com/2/users/by", params=params
+# )
+
+response = oauth.get(
+    create_url(), params=get_params()
+)
+
+
+if response.status_code != 200:
+    raise Exception(
+        "Request returned an error: {} {}".format(response.status_code, response.text)
+    )
+
+print("Response code: {}".format(response.status_code))
+json_response = response.json()
+print(json.dumps(json_response, indent=4, sort_keys=True))
